a71bcfed2ac74dedf8b1e7ad796aea8fbb1ef378
Álvaro Kenzo <alvaroymagawa@gmail.com>
2019-06-20 18:39:20 -0300

    TelaPrincipal added

--- client/CryptoFile/src/Control/Main.java
+++ client/CryptoFile/src/Control/Main.java
@@ -10,21 +10,41 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
     
-    public static Stage stanceStage;
+    private static Stage stanceStage;
     public static Scene loginScene;
+    public static Scene mainScreenScene;
     
     @Override
     public void start(Stage stage) throws Exception {
+        stanceStage = stage;
         //Fazer isso para cada nova interface criada
-        Parent loginFxml = FXMLLoader.load(getClass().getResource("/View/Login.fxml"));
+        Parent loginFxml = FXMLLoader.load(getClass().getResource("/View/TelaLogin.fxml"));
         loginScene = new Scene(loginFxml);
+        Parent mainScreenFxml = FXMLLoader.load(getClass().getResource("/View/TelaPrincipal.fxml"));
+        mainScreenScene = new Scene(mainScreenFxml);
         //-------------------------------
         stage.initStyle(StageStyle.UNDECORATED);
        
         stage.setScene(loginScene);//Mostra a tela desejada
         stage.show();
     }
-
+    
+    public static void changeScreen(String screen){
+        switch(screen){
+            case "TelaLogin":
+                stanceStage.setScene(loginScene);
+            case "TelaPrincipal":
+                stanceStage.setScene(mainScreenScene);
+        }
+    }
+    
+    public static void closeScreen(){
+        stanceStage.close();
+    }
+    
+    public static void minimizeScreen(){
+        stanceStage.setIconified(true);
+    }
     
     public static void main(String[] args) {
         launch(args);
---
+++ client/CryptoFile/src/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kenzo
+ */
+public class NewClass {
+    public static String p = "B";
+    public static int i = Integer.parseInt(p);
+    
+    public static void main(String[] args) {
+        
+        System.out.println(i);
+    }
+}
